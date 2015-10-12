8027d3c6f196506c74b7771b8e4a33eb0922a340
Tyler Conlee <tylerlconlee@gmail.com>
2015-10-12 15:24:02 -0700

    added page titles

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', { title: 'Tyler Conlee - Developer' });
 });
 app.get('/about', function (req, res) {
-    res.render('about');
+    res.render('about', { title: 'Tyler Conlee - Developer' });
 });
 app.get('/portfolio', function (req, res) {
-    res.render('portfolio');
+    res.render('portfolio', { title: 'Tyler Conlee - Developer' });
 });
 app.get('/contact', function (req, res) {
-    res.render('contact');
+    res.render('contact', { title: 'Tyler Conlee - Developer' });
 });
 
 app.listen(3000);
