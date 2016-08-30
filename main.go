fb4b1d3df6e90b087d4103e60c40d09d5f4cb800
Tyler Conlee <tylerlconlee@gmail.com>
2016-08-30 14:38:23 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/newrelic/go-agent"
+	"github.com/newrelic/go-agent/_integrations/nrmux"
+)
+
+func main() {
+	var nrkey string
+	var nrappname string
+	flag.StringVar(&nrkey, "nrkey", "null", "New Relic License Key")
+	flag.StringVar(&nrappname, "nrappname", "Go Application", "New Relic Application Name")
+	flag.Parse()
+
+	config := newrelic.NewConfig(nrappname, nrkey)
+	app, err := newrelic.NewApplication(config)
+	if nil != err {
+		log.Fatal(err.Error())
+	}
+	r := nrmux.New(app)
+	r.HandleFunc("/", HomeHandler)
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Hello world")
+}
